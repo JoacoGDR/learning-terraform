5240ae7bcbb3ca87a672431596621311edd69d0e
Joaquin Garcia del Rio <joaquin.garciadelrio@innovid.com>
2025-10-11 17:08:20 -0300

    Creates VPC module
    Creates a VPC module using terraform-aws-modules/vpc/aws.
    
    The module creates public subnets in the specified availability zones, allowing for the deployment of resources within the VPC.
    
    The security group now references the created VPC.

--- main.tf
+++ main.tf
@@ -14,9 +14,19 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
+module "web_vpc" {
+  source = "terraform-aws-modules/vpc/aws"
 
-data "aws_vpc" "default" {
-  default = true
+  name = "dev"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
+
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
 }
 
 resource "aws_instance" "web" {
@@ -24,6 +34,8 @@ resource "aws_instance" "web" {
   instance_type = var.instance_type
 
   vpc_security_group_ids = [module.web_security_group.security_group_id]
+
+  subnet_id = module.web_vpc.public_subnets[0]
 
   tags = {
     Name = "Learning Terraform"
@@ -35,7 +47,7 @@ module "web_security_group" {
   version = "5.3.0"
   name = "web"
 
-  vpc_id = data.aws_vpc.default.id
+  vpc_id = module.web_vpc.vpc_id
 
   ingress_rules = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
