9d661f6eb15751ddf01e421ba33fcd38abf1a36b
Joaquin Garcia del Rio <joaquin.garciadelrio@innovid.com>
2025-10-11 16:30:44 -0300

    Refactors security group implementation
    Replaces individual security group resources with a module.
    This provides a more concise and manageable configuration.

--- main.tf
+++ main.tf
@@ -23,42 +23,23 @@ resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-  vpc_security_group_ids = [aws_security_group.web.id]
+  vpc_security_group_ids = [module.web_security_group.security_group_id]
 
   tags = {
     Name = "Learning Terraform"
   }
 }
 
-resource "aws_security_group" "web" {
+module "web_security_group" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "5.3.0"
   name = "web"
-  description = "Allow http and https in. Allow everything out."
+
   vpc_id = data.aws_vpc.default.id
-}
 
-resource "aws_security_group_rule" "web_http_in" {
-  type = "ingress"
-  from_port = 80
-  to_port = 80
-  protocol = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.web.id
-}
+  ingress_rules = ["http-80-tcp", "https-443-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
 
-resource "aws_security_group_rule" "web_https_in" {
-  type = "ingress"
-  from_port = 443
-  to_port = 443
-  protocol = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.web.id
-}
-
-resource "aws_security_group_rule" "web_everything_out" {
-  type = "egress"
-  from_port = 0
-  to_port = 0
-  protocol = "-1"
-  cidr_blocks = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.web.id
+  egress_rules = ["all-all"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
 }
