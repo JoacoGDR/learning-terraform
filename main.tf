837a36f3c119dd6a721d3adb680ea2ced40aec4b
Joaquin Garcia del Rio <joaquin.garciadelrio@innovid.com>
2025-10-18 23:06:47 -0300

    autoscaling

--- main.tf
+++ main.tf
@@ -30,46 +30,51 @@ module "web_vpc" {
   }
 }
 
-resource "aws_instance" "web" {
-  ami           = data.aws_ami.app_ami.id
+module "autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "6.5.2"
+
+  name = "web"
+  min_size = var.scaling_min_size
+  max_size = var.scaling_max_size
+
+  vpc_zone_identifier = module.web_vpc.public_subnets
+  target_group_arns = module.web_alb.target_group_arns
+  security_groups = [module.web_security_group.security_group_id]
+
+  image_id           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
-
-  vpc_security_group_ids = [module.web_security_group.security_group_id]
-
-  subnet_id = module.web_vpc.public_subnets[0]
-
-  tags = {
-    Name = "Learning Terraform"
-  }
 }
 
-module "alb" {
+module "web_alb" {
   source = "terraform-aws-modules/alb/aws"
+  version = "~> 6.0"
 
   name    = "web-alb"
+  load_balancer_type = "application"
+
   vpc_id  = module.web_vpc.vpc_id
   subnets = module.web_vpc.public_subnets
   security_groups = [module.web_security_group.security_group_id]
 
-  target_groups = {
-    web-instance = {
+
+  target_groups = [
+    {
+      name = "web-instance"
       name_prefix      = "web-"
       protocol         = "HTTP"
-      port             = 80
+      port             = 8080
       target_type      = "instance"
-      target_id        = aws_instance.web.id
     }
-  }
+  ]
 
-  listeners = {
-    http = {
+  http_tcp_listeners = [
+    {
       port = 80
       protocol = "HTTP"
-      forward = {
-        target_group_key = "web-instance"
-      }
+      target_group_index = 0
     }
-  }
+  ]
 
   tags = {
     Environment = "dev"
--- variables.tf
+++ variables.tf
@@ -2,3 +2,41 @@ variable "instance_type" {
   description = "Type of EC2 instance to provision"
   default     = "t3.nano"
 }
+
+variable "ami_filter" {
+  description = "Name filter and owner for AMI"
+
+  type = object({
+    name = string
+    owner = string
+  })
+
+  default = {
+    name = "bitnami-tomcat-*-x86_64-hvm-ebs-nami"
+    owner = "979382823631"
+  }
+}
+
+variable "environment" {
+  description = "Development Environment"
+
+  type = object({
+    name = string
+    network_prefix = string
+  })
+
+  default = {
+    name = "dev"
+    network_prefix = "10.0"
+  }
+}
+
+variable "scaling_min_size" {
+  description = "Minimum size of the autoscaling group"
+  default     = 1
+}
+
+variable "scaling_max_size" {
+  description = "Maximum size of the autoscaling group"
+  default     = 2
+}
