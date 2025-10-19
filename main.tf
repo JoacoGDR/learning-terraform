2dabfac46581bd310b2a22e0195784210b674c77
Joaquin Garcia del Rio <joaquin.garciadelrio@innovid.com>
2025-10-18 23:11:17 -0300

    bump version of autoscaling

--- main.tf
+++ main.tf
@@ -32,7 +32,7 @@ module "web_vpc" {
 
 module "autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
-  version = "6.5.2"
+  version = "~> 7.0"
 
   name = "web"
   min_size = var.scaling_min_size
