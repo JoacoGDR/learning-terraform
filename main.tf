1db8a56d86411c8f1efe3d0805eaa9d8a6f82c27
Joaquin Garcia del Rio <joaquin.garciadelrio@innovid.com>
2025-10-18 21:50:37 -0300

    fix 2

--- main.tf
+++ main.tf
@@ -63,14 +63,15 @@ module "alb" {
       }
     }
   }
-
-  http_tcp_listeners = [
-    {
-      port = 80
-      protocol = "HTTP"
-      target_group_index = 0
-    }
-  ]
+  listeners = {
+    http_tcp_listeners = [
+      {
+        port = 80
+        protocol = "HTTP"
+        target_group_index = 0
+      }
+    ]
+  }
 
   tags = {
     Environment = "dev"
